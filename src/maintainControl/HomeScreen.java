f99dc2a8480ba2744b4eb5f928e1fc19d49eea84
Savannah <91633941+lovelysandlonelys@users.noreply.github.com>
2023-04-21 16:41:50 -0500

    restructuring

---
+++ src/maintainControl/HomeScreen.java
@@ -0,0 +1,18 @@
+/////////////////////////////////////////
+//Program Name: HomeScreen.java
+//Program Purpose: create a jFrame for the home screen of Maintain Control
+//Last Updated: 04/21/2023
+//Last Updated By: Savannah Stephenson
+/////////////////////////////////////////
+
+//Prckage
+package maintainControl;
+
+//Imports
+import javax.swing.JFrame;
+
+//Start of HomeScreen Class
+public class HomeScreen extends JFrame
+{
+    
+}//End of HomeScreen Class
---
+++ src/maintainControl/MaintainControlDriver.java
@@ -0,0 +1,27 @@
+/////////////////////////////////////////
+//Program Name: 
+//Program Purpose: 
+//Last Updated: 04/21/2023
+//Last Updated By: Savannah Stephenson
+/////////////////////////////////////////
+
+//Package
+package maintainControl;
+
+//Imports 
+import javax.swing.*;
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+//Start of MaintainControDriver Class
+public class MaintainControlDriver
+{
+	//Main
+	public static void main(String args[])
+	{
+
+		
+	}//End of main
+
+}//End of MaintainControlDriver class
